c8188132530623aaad829dcf61fd240e5ddde2f6
Alexandra Putuntica <alexandra.putuntica@print.com>
2022-10-14 15:57:29 +0100

    UI-3 Props

--- App.js
+++ App.js
@@ -2,10 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ChatListItem from './src/components/ChatListItem';
 
+const chat = {
+  id: '1',
+  user: {
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
+    name: 'Lukas',
+  },
+  lastMessage: {
+    text: 'Oke',
+    createdAt: '07:30',
+  },
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem />
+      <ChatListItem chat={chat} />
 
       <StatusBar style="auto" />
     </View>
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,23 +1,20 @@
 import { Text, View, Image, StyleSheet } from 'react-native';
 
-const ChatListItem = () => {
+const ChatListItem = ({ chat }) => {
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg' }}
-        style={styles.image}
-      />
+      <Image source={{ uri: chat.user.image }} style={styles.image} />
 
       <View style={styles.content}>
         <View style={styles.row}>
           <Text style={styles.name} numberOfLines={1}>
-            Lukas
+            {chat.user.name}
           </Text>
-          <Text style={styles.subTitle}>8:30</Text>
+          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
         </View>
 
         <Text numberOfLines={2} style={styles.subTitle}>
-          Hello theredsf
+          {chat.lastMessage.text}
         </Text>
       </View>
     </View>
