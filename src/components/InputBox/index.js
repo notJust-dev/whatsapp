c00c9d3cdd6852bcf2268ca5cc5ee5193e1fcec3
Alexandra Putuntica <alexandra.putuntica@print.com>
2022-10-14 17:06:31 +0100

    UI-6 Input Box

---
+++ src/components/InputBox/index.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+
+const InputBox = () => {
+  const [newMessage, setNewMessage] = useState('');
+
+  const onSend = () => {
+    console.warn('Sending a new message: ', newMessage);
+
+    setNewMessage('');
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Icon */}
+      <AntDesign name="plus" size={20} color="royalblue" />
+
+      {/* Text Input */}
+      <TextInput
+        value={newMessage}
+        onChangeText={setNewMessage}
+        style={styles.input}
+        placeholder="type your message..."
+      />
+
+      {/* Icon */}
+      <MaterialIcons onPress={onSend} style={styles.send} name="send" size={16} color="white" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: 'whitesmoke',
+    padding: 5,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 5,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
+
+    borderRadius: 50,
+    borderColor: 'lightgray',
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  send: {
+    backgroundColor: 'royalblue',
+    padding: 7,
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+});
+
+export default InputBox;
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,19 +1,23 @@
-import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native';
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from 'react-native';
 import Message from '../components/Message';
+import InputBox from '../components/InputBox';
 
 import bg from '../../assets/images/BG.png';
 import messages from '../../assets/data/messages.json';
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-        style={styles.list}
-        inverted
-      />
-    </ImageBackground>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.bg}>
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
